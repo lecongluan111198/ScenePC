8c6f67b21d6c683f64c8a5a44716cd32d3e85e5d
hvtham <viettham1998@gmail.com>
2020-04-12 15:08:16 +0700

    Save & Load file using SimpleJSON

---
+++ Assets/Scripts/EditContext.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using UnityEngine.UI;
+using System;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+public class EditContext : MonoBehaviour
+{
+    public Text textM;
+    public Text userNameText;
+    int i = 0;
+//    public void Request()
+//    {
+//        i++;
+//        userNameText.text = "" + i;
+//        string jsonfile = "abcbsbdb"; //nhap string
+//        WWW request = new WWW(jsonfile);
+//        PlayerPrefs.SetInt("authComplete", 0);
+//        textM.text = "Requested2";
+//        StartCoroutine(OnResponse(request));
+//    }
+//    private IEnumerator OnResponse(WWW req)
+//    {
+//        yield return req;
+//        textM.text = req.text; //Response Json
+//        var rt = JsonConvert.DeserializeObject<RootObject>(req.text);
+//        string nameObj = rt.jsonData.Scene.name;
+//        userNameText.text = nameObj + ":" + i;
+//        string linkObj = rt.jsonData.Scene.link;
+//        StartCoroutine(loadObject(linkObj));
+//    }  
+//    IEnumerator loadObject(string url)
+//    {
+//        if (Application.internetReachability == NetworkReachability.NotReachable)
+//        {
+//            yield return null;
+//        }
+//        var www = new WWW(url);
+//        Debug.Log("Download obj on progress");
+//        yield return www;
+//        if (string.IsNullOrEmpty(www.text))
+//            Debug.Log("Download Failed");
+//        else
+//        {
+//            //code download obj
+//            Debug.Log("Download Success");
+//        }
+//    }
+    public void GetJsonData()
+    {
+        StartCoroutine(rRequestWebService());
+    }
+    IEnumerator rRequestWebService()
+    {
+        string getDataUrl = "https://drive.google.com/uc?export=download&id=13GU3xtSNds0t4UcneXxoA5RnREkoZkjL"; //duong dan
+        print(getDataUrl);
+        using(UnityWebRequest webData = UnityWebRequest.Get(getDataUrl))
+        {
+            yield return webData.SendWebRequest();
+            if (webData.isNetworkError || webData.isHttpError)
+            {
+                print("Error");
+                print(webData.error);
+            }
+            else
+            {
+                if (webData.isDone)
+                {
+                    JSONNode jsonData = JSON.Parse(System.Text.Encoding.UTF8.GetString(webData.downloadHandler.data));
+                    if (jsonData == null)
+                    {
+                        print("No Data");
+                    }
+                    else
+                    {
+                        print("Json Data");
+                        print("Count:" + jsonData.Count);
+                    }
+                }
+            }
+        }
+    }
+}
+[Serializable]
+public class Question
+{
+    public string question { get; set; }
+    public string choiceA { get; set; }
+    public string choiceB { get; set; }
+    public string choiceC { get; set; }
+    public string correct { get; set; }
+}
+[Serializable]
+public class Component
+{
+    public List<Question> questions { get; set; }
+}
+[Serializable]
+public class Scene
+{
+    public string id { get; set; }
+    public string link { get; set; }
+    public string name { get; set; }
+    public List<int> position { get; set; }
+    public List<int> rotation { get; set; }
+    public Component component { get; set; }
+}
+[Serializable]
+public class JsonData
+{
+    public Scene Scene { get; set; }
+}
+[Serializable]
+public class RootObject
+{
+    public JsonData jsonData { get; set; }
+}
---
+++ Assets/Scripts/JsonSave.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using SimpleJSON;
+public class JsonSave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Id;
+    public string Name;
+    void Save()
+    {
+        //info
+        JSONObject playerJson = new JSONObject();
+        playerJson.Add("Id", Id);
+        playerJson.Add("Name", Name);
+        //Position
+        JSONArray position = new JSONArray();
+        position.Add(transform.position.x);
+        position.Add(transform.position.y);
+        position.Add(transform.position.z);
+        playerJson.Add("Position", position);
+        //Rotation
+        JSONArray rotation = new JSONArray();
+        rotation.Add(transform.rotation.x);
+        rotation.Add(transform.rotation.y);
+        rotation.Add(transform.rotation.z);
+        playerJson.Add("Rotation", rotation);
+        //Scale
+        JSONArray scale = new JSONArray();
+        scale.Add(transform.localScale.x);
+        scale.Add(transform.localScale.y);
+        scale.Add(transform.localScale.z);
+        playerJson.Add("Scale", scale);
+        //Save json to laptop
+        string path = Application.persistentDataPath + "/data.json";
+        Debug.Log(path);
+        File.WriteAllText(path, playerJson.ToString());
+    }
+    void Load()
+    {
+        //Read from file and load
+        string path = Application.persistentDataPath + "/data.json";
+        string jsonString = File.ReadAllText(path);
+        JSONObject playerJson = (JSONObject)JSON.Parse(jsonString);
+        //Set value
+        Name = playerJson["Name"];
+        Id = playerJson["Id"];
+        //Position
+        transform.position = new Vector3(
+            playerJson["Position"].AsArray[0],
+            playerJson["Position"].AsArray[1],
+            playerJson["Position"].AsArray[2]
+            );
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)) Save();
+        if (Input.GetKeyDown(KeyCode.L)) Load();
+    }
+}
