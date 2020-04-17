19a97ad05f963e14f517b13a97e1687e832f90a7
hvtham <viettham1998@gmail.com>
2020-04-17 08:55:29 +0700

    Save file

--- Assets/Scripts/EditContext.cs
+++ Assets/Scripts/EditContext.cs
@@ -10,112 +10,10 @@ using SimpleJSON;
 
 public class EditContext : MonoBehaviour
 {
-    public int Id;
-    public string name = "abc";
-    List<Obj> listObj = new List<Obj>();
-    Scene listScene = new Scene();
-    public void addListObj(Transform transform)
-    {   
-        var objtmp = new Obj();
-        objtmp.ID = Id;
-        objtmp.Name = transform.name;
-        objtmp.Position = transform.position;
-        objtmp.Scale = transform.localScale;
-        listObj.Add(objtmp);
-        Debug.Log("listObj: " + listObj);
-
-        //var xyz = new Scene {
-        //    scene = new Dictionary<string, Obj>
-        //    {
-        //        {"Obj", objtmp }
-        //    }
-        //};
-
-        //listScene.scene = new Dictionary<string, Obj>
-        //{
-        //    {"Obj", objtmp }
-        //};
-    }
-    public void SaveScene()
-    {
-        var mno = new Scenes
-        {
-            scenes = new Dictionary<string, Scene>
-            {
-                {"scene1",listScene }
-            }
-        };
-    }
-    public void writeToJson()
-    {
-        //var setting = new JsonSerializerSettings();
-        //setting.Formatting = Formatting.Indented;
-        //setting.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-
-        //// write
-        //// var accountsFromCode = new List<Account> { accountJames, accountOnion };
-        //// var accountsFromCode = new List<List<List<List<Obj>>>> {data};
-        //var scenesFromCode = new List<List<Obj>> { listObj };
-        //var json = JsonConvert.SerializeObject(scenesFromCode, setting);
-        //var path = Path.Combine(Application.dataPath, "hiiii.json");
-        //Debug.Log(path);
-        //File.WriteAllText(path, json);
-    }
-    public void readFromJson()
-    {
-        //var setting = new JsonSerializerSettings();
-        //setting.Formatting = Formatting.Indented;
-        //setting.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-        //var path = Path.Combine(Application.dataPath, "hiiii.json");
-        //var fileContent = File.ReadAllText(path);
-        //var objFromFile = JsonConvert.DeserializeObject<List<List<Obj>>>(fileContent);
-        //var reSerializedJson = JsonConvert.SerializeObject(objFromFile, setting);
-        //print(reSerializedJson);
-    }
-
-
-    public class Obj
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public Vector3 Position { get; set; }
-        public Vector3 Rotation { get; set; }
-        public Vector3 Scale { get; set; }
-        public IList<string> Component { get; set; }
-    }
-    public class Scene
-    {
-        public Dictionary<string, Obj> scene { get; set; }
-    }
-    public class Scenes
-    {
-        public Dictionary<string, Scene> scenes { get; set; }
-    }
-    //public class Data
-    //{
-    //    int phrase;
-    //    public IList<Scenes> data { get; set; }
-    //}
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.S))
-            writeToJson();
-        if (Input.GetKeyDown(KeyCode.L))
-            readFromJson();
-    }
-
     void Start()
     {
         json();
-        //WithForeachLoop();
-    }
-    void WithForeachLoop()
-    {
-        foreach (Transform child in transform)
-        {
-            print("Object con: " + child);
-            addListObj(child);
-        }
+        //loadJson();
     }
 
     public class JsonObject
@@ -123,15 +21,24 @@ public class EditContext : MonoBehaviour
         int id;
         string name;
         float[] position;
-        float[] rotation;
+        float[] scale;
         List<Dictionary<string, object>> components;
+        
+        public float[] vector3ToFloat(Vector3 vt)
+        {
+            float[] tmp = new float[3];
+            tmp[0] = vt.x;
+            tmp[1] = vt.y;
+            tmp[2] = vt.z;
+            return tmp;
+        }
 
-        public JsonObject(int id, string name, float[] position, float[] rotation, List<Dictionary<string, object>> components)
+        public JsonObject(int id, string name, Vector3 position, Vector3 scale, List<Dictionary<string, object>> components)
         {
             this.id = id;
             this.name = name;
-            this.position = position;
-            this.rotation = rotation;
+            this.position = vector3ToFloat(position);
+            this.scale = vector3ToFloat(scale);
             this.components = components;
         }
 
@@ -141,7 +48,7 @@ public class EditContext : MonoBehaviour
             dic.Add("id", id);
             dic.Add("name", name);
             dic.Add("position", position);
-            dic.Add("rotation", rotation);
+            dic.Add("scale", scale);
             dic.Add("components", components);
             return dic;
         }
@@ -151,25 +58,62 @@ public class EditContext : MonoBehaviour
     {
         Dictionary<string, object> jsonData = new Dictionary<string, object>();
         List<Dictionary<string, object>> listScenes = new List<Dictionary<string, object>>();
-        jsonData.Add("phase", 5);
-        jsonData.Add("Scenes", listScenes);
+        jsonData.Add("nPhrase", 1);
+        jsonData.Add("Phrase", listScenes);
         Dictionary<string, object> scene = new Dictionary<string, object>();
         listScenes.Add(scene);
 
-        scene.Add("nObject", 2);
+        scene.Add("nObject", transform.childCount);
 
         List<Dictionary<string, object>> objects = new List<Dictionary<string, object>>();
         JsonObject jsonObject;
-        for (int i = 0; i < 2; i++)
+      
+        foreach (Transform child in transform)
         {
-            jsonObject = new JsonObject(i, "tham", new float[] { 1, 2, 3 }, new float[] { 1, 2, 3 }, new List<Dictionary<string, object>>());
-            Debug.Log(JsonConvert.SerializeObject(jsonObject));
+            print("Object con: " + child);
+            jsonObject = new JsonObject(0, child.name, child.localPosition, child.localScale, new List<Dictionary<string, object>>());
+            //Debug.Log(JsonConvert.SerializeObject(jsonObject));
             objects.Add(jsonObject.toJson());
         }
         scene.Add("Objects", objects);
-
         Debug.Log(JsonConvert.SerializeObject(jsonData));
+        var path = Path.Combine(Application.dataPath, "data.json");
+        Debug.Log(path);
+        File.WriteAllText(path, JsonConvert.SerializeObject(jsonData));
+    }
+    public void loadJson()
+    {
+        var path = Path.Combine(Application.dataPath, "data.json");
+        var fileContent = File.ReadAllText(path);
+        Debug.Log(JsonConvert.DeserializeObject(fileContent));
+        Dictionary<string, object> jsonData = JsonConvert.DeserializeObject<Dictionary<string, object>>(fileContent);
+        Debug.Log(jsonData["nPhrase"]);
+        Debug.Log(jsonData["Phrase"]);
 
+        List<Dictionary<string, object>> listScenes = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>((jsonData["Phrase"]).ToString());
+        Debug.Log(listScenes[0]);
+        Debug.Log(string.Join(",",listScenes));
+        //Debug.Log(JsonConvert.DeserializeObject<Dictionary<string, object>>((listScenes.ToArray()).ToString()));
+        //Debug.Log(string.Join(",", listScenes.ToArray()));
+        //new List<Dictionary<string, object>>();
+        //jsonData.Add("nPhrase", 1);
+        //jsonData.Add("Phrase", listScenes);
+        //Debug.Log(JsonConvert.DeserializeObject < Dictionary<string, object>>(string.Join(",", listScenes)));
+        //Dictionary<string, object> scene = JsonConvert.DeserializeObject<Dictionary<string, object>>((listScenes[0]).ToString());
+        //listScenes.Add(scene);
+        //Debug.Log(scene["nObject"]);
+        //scene.Add("nObject", transform.childCount);
+
+        //List<Dictionary<string, object>> objects = new List<Dictionary<string, object>>();
+        //JsonObject jsonObject;
+
+        //foreach (Transform child in transform)
+        //{
+        //    print("Object con: " + child);
+        //    jsonObject = new JsonObject(0, child.name, child.localPosition, child.localScale, new List<Dictionary<string, object>>());
+        //    objects.Add(jsonObject.toJson());
+        //}
+        //scene.Add("Objects", objects);
     }
 }
 
