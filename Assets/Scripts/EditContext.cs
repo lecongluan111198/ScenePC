b2a0bb0b2867bdeb81a1107e3f38a6ae627ce526
hvtham <viettham1998@gmail.com>
2020-04-17 15:10:06 +0700

    Load file

--- Assets/Scripts/EditContext.cs
+++ Assets/Scripts/EditContext.cs
@@ -12,8 +12,8 @@ public class EditContext : MonoBehaviour
 {
     void Start()
     {
-        json();
-        //loadJson();
+        //json();
+        loadJson();
     }
 
     public class JsonObject
@@ -42,6 +42,11 @@ public class EditContext : MonoBehaviour
             this.components = components;
         }
 
+        public JsonObject()
+        {
+
+        }
+
         public Dictionary<string, object> toJson()
         {
             Dictionary<string, object> dic = new Dictionary<string, object>();
@@ -52,17 +57,17 @@ public class EditContext : MonoBehaviour
             dic.Add("components", components);
             return dic;
         }
+        //Hàm gán giá trị lại cho obj
     }
 
     public void json()
     {
         Dictionary<string, object> jsonData = new Dictionary<string, object>();
         List<Dictionary<string, object>> listScenes = new List<Dictionary<string, object>>();
+        Dictionary<string, object> scene = new Dictionary<string, object>();
         jsonData.Add("nPhrase", 1);
         jsonData.Add("Phrase", listScenes);
-        Dictionary<string, object> scene = new Dictionary<string, object>();
         listScenes.Add(scene);
-
         scene.Add("nObject", transform.childCount);
 
         List<Dictionary<string, object>> objects = new List<Dictionary<string, object>>();
@@ -72,7 +77,7 @@ public class EditContext : MonoBehaviour
         {
             print("Object con: " + child);
             jsonObject = new JsonObject(0, child.name, child.localPosition, child.localScale, new List<Dictionary<string, object>>());
-            //Debug.Log(JsonConvert.SerializeObject(jsonObject));
+            Debug.Log(JsonConvert.SerializeObject(jsonObject));
             objects.Add(jsonObject.toJson());
         }
         scene.Add("Objects", objects);
@@ -83,37 +88,18 @@ public class EditContext : MonoBehaviour
     }
     public void loadJson()
     {
-        var path = Path.Combine(Application.dataPath, "data.json");
-        var fileContent = File.ReadAllText(path);
-        Debug.Log(JsonConvert.DeserializeObject(fileContent));
-        Dictionary<string, object> jsonData = JsonConvert.DeserializeObject<Dictionary<string, object>>(fileContent);
-        Debug.Log(jsonData["nPhrase"]);
-        Debug.Log(jsonData["Phrase"]);
-
-        List<Dictionary<string, object>> listScenes = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>((jsonData["Phrase"]).ToString());
-        Debug.Log(listScenes[0]);
-        Debug.Log(string.Join(",",listScenes));
-        //Debug.Log(JsonConvert.DeserializeObject<Dictionary<string, object>>((listScenes.ToArray()).ToString()));
-        //Debug.Log(string.Join(",", listScenes.ToArray()));
-        //new List<Dictionary<string, object>>();
-        //jsonData.Add("nPhrase", 1);
-        //jsonData.Add("Phrase", listScenes);
-        //Debug.Log(JsonConvert.DeserializeObject < Dictionary<string, object>>(string.Join(",", listScenes)));
-        //Dictionary<string, object> scene = JsonConvert.DeserializeObject<Dictionary<string, object>>((listScenes[0]).ToString());
-        //listScenes.Add(scene);
-        //Debug.Log(scene["nObject"]);
-        //scene.Add("nObject", transform.childCount);
-
-        //List<Dictionary<string, object>> objects = new List<Dictionary<string, object>>();
-        //JsonObject jsonObject;
-
-        //foreach (Transform child in transform)
-        //{
-        //    print("Object con: " + child);
-        //    jsonObject = new JsonObject(0, child.name, child.localPosition, child.localScale, new List<Dictionary<string, object>>());
-        //    objects.Add(jsonObject.toJson());
-        //}
-        //scene.Add("Objects", objects);
+        var path = Path.Combine(Application.dataPath, "a.json");
+        var jsonData = File.ReadAllText(path);
+        Debug.Log(JsonConvert.DeserializeObject(jsonData));
+        Dictionary<string, object> listScenes = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonData);
+        Debug.Log(listScenes["Phrase"]);
+        //chạy vòng for (i=0;i<nPhrase;i++)
+        List<Dictionary<string, object>> scenes = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(listScenes["Phrase"].ToString());
+        Debug.Log(scenes[0]["Objects"]);
+        Debug.Log(scenes[1]["Objects"]);
+        //Chạy vòng for(j=0;j<nObject;i++)
+        Dictionary<string, object> objects = JsonConvert.DeserializeObject<Dictionary<string, object>>(scenes[0]["Objects"].ToString());
+        //load đối tượng và gán giá trị
     }
 }
 
