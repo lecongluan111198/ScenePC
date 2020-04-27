9f8cbbb58801d07cb86b15024aa78ab10c330f46
hvtham <viettham1998@gmail.com>
2020-04-27 23:51:04 +0700

    Move + Scale

---
+++ Assets/Scripts/ActionManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionManager : MonoBehaviour
+{
+    private bool isMove = false;
+    private bool isScale = false;
+    public bool IsMove { get => isMove; set => isMove = value; }
+    public bool IsScale { get => isScale; set => isScale = value; }
+
+    public void Move()
+    {
+        IsScale = false;
+        IsMove = !IsMove;
+    }
+    public void Scale()
+    {
+        IsMove = false;
+        IsScale = !IsScale;
+    }
+}
--- Assets/Scripts/Context/Components/AbstractComponent.cs
+++ Assets/Scripts/Context/Components/AbstractComponent.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class AbstractComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ObjectComponent
     {
-        
+        public int id { get; set; }
+        public string name { get; set; }
+        public ObjectComponent(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        public ObjectComponent()
+        {
+            id = 0;
+            name = "";
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public class QuestionComponent : ObjectComponent
     {
+        public string question { get; set; }
+        public List<string> choose { get; set; }
+        public string answer { get; set; }
+        public QuestionComponent(string question, List<string> choose, string answer)
+        {
+            this.question = question;
+            this.choose = choose;
+            this.answer = answer;
+        }
+    }
+    private void loadQuestion()
+    {
+
+    }
+    public void loadComponent()
+    {
+        ObjectComponent objC = new ObjectComponent();
+        switch (objC.id)
+        {
+            case 1:
+                loadQuestion();
+                break;
+            case 2:
+                
+                break;
+            default:
+                Debug.Log("ID Component not found");
+                break;
+        }
         
     }
 }
