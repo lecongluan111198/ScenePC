fbbb39b9588b700a022ce37e02e7abb13dac79eb
hvtham <viettham1998@gmail.com>
2020-06-15 19:51:00 +0700

    Update UI Settings

---
+++ Assets/Scripts/Handlers/Panels/VolumeChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeChange : MonoBehaviour
+{
+    private AudioSource AudioSrc;
+    public Slider slider; 
+    private float AudioVolume = 0.5f;
+
+    void Start()
+    {
+        AudioSrc = GetComponent<AudioSource>();
+    }
+
+    public void Update()
+    {
+        AudioSrc.volume = AudioVolume;
+    }
+
+    public void SetVolume()
+    {
+        AudioVolume = (slider.value)/100;
+    }
+}
