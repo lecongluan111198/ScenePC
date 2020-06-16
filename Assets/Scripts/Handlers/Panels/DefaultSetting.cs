f9e1b0d3fe6a9c4cdc6a9ed6bda3e781f94f299d
hvtham <viettham1998@gmail.com>
2020-06-16 10:25:36 +0700

    Default Setting Button

---
+++ Assets/Scripts/Handlers/Panels/DefaultSetting.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DefaultSetting : MonoBehaviour
+{
+    [Header("MUSIC")]
+    public Slider musicSlider;
+    [Header("SOUND")]
+    public Slider soundSlider;
+    private float defaultSlider = 50.0f;
+    public void setDefault()
+    {
+        musicSlider.value = defaultSlider;
+        soundSlider.value = defaultSlider;
+    }
+}
