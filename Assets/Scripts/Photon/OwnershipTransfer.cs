ec599d28679eb9d2ee45419ef022220b35e7067f
LuanLee <lecongluan111198@gmail.com>
2020-06-18 00:01:39 +0700

    add ownership transfer

---
+++ Assets/Scripts/Photon/OwnershipTransfer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwnershipTransfer : MonoBehaviourPun, IMixedRealityPointerHandler
+{
+    private void OnMouseDown()
+    {
+        base.photonView.RequestOwnership();
+    }
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        base.photonView.RequestOwnership();
+    }
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+       
+    }
+
+}
